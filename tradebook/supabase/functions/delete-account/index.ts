4b55c3923b7a7cf70f83471bfa11dea6fb79302a
judo <countingthis@gmail.com>
2026-03-24 21:23:19 -0400

    fixdeletev2

--- tradebook/supabase/functions/delete-account/index.ts
+++ tradebook/supabase/functions/delete-account/index.ts
@@ -1,9 +1,9 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.0";
-import Stripe from "https://esm.sh/stripe@13.10.0?target=deno&no-check";
+import Stripe from "https://esm.sh/stripe@17.7.0?target=denonext";
 
 const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY")!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-12-18.acacia",
   httpClient: Stripe.createFetchHttpClient(),
 });
 
